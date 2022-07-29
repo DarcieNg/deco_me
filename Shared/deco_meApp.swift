3d042c0aa83122bbf9c039b7869f5b2f1659db57
DarcieNg <s3865443@rmit.edu.vn>
2022-07-29 14:59:14 +0700

    Initial Commit

---
+++ Shared/deco_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  deco_meApp.swift
+//  Shared
+//
+//  Created by Nguyễn Vũ Thuỳ Dương on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct deco_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
